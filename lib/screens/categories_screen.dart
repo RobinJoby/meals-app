ce960af5b7ed3a0cf41da1011a1bdb92fbbda4cd
Robin <robinjoby2004@gmail.com>
2024-07-06 14:31:48 +0530

    explicit animation added

--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -77,9 +77,16 @@ class _CategoriesScreenState extends State<CategoriesScreen>
           ],
         ),
         builder: (context, child) {
-          return Padding(
-            padding:
-                EdgeInsets.only(top: 100 - _animationController.value * 100),
+          return SlideTransition(
+            position: Tween(
+              begin: const Offset(0, 0.3),
+              end: const Offset(0, 0),
+            ).animate(
+              CurvedAnimation(
+                parent: _animationController,
+                curve: Curves.easeInOut,
+              ),
+            ),
             child: child,
           );
         });
